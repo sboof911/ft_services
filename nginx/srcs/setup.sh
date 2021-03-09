68270aabc63f716e39b0bcfac16a9884d47aebcc
Anass Maach <amaach@e2r6p8.1337.ma>
2021-03-09 15:54:08 +0100

    nginx good

--- nginx/srcs/setup.sh
+++ nginx/srcs/setup.sh
@@ -1,15 +1,11 @@
 apk update
-apk add nginx
-adduser -S -g 'Nginx www user' -h /home/www/ sboof
-mv /home/srcs/default.conf /etc/nginx/conf.d/
-mkdir /etc/ssl/nginx
-apk add openssl
-openssl req -nodes -new -x509 -keyout /etc/ssl/nginx/nginx.key -out /etc/ssl/nginx/nginx.crt -days 365 -subj "/C=Mo/ST=Oregon/L=Portland/O=ft_services/OU=Org/CN=www.sboof.com"
-chmod 0600 /etc/ssl/nginx
-chmod 777 /sys
-apk add openrc
-mkdir /run/nginx
-nginx -t
+apk add openrc nginx openssl
 openrc reboot
-#service nginx start
-#echo "sboof was here !!!" > home/www/index.html
+adduser -S -g 'Nginx www user' -h /home/www/ sboof
+mkdir /etc/ssl/nginx
+openssl req -nodes -new -x509 -keyout /etc/ssl/nginx/nginx.key -out /etc/ssl/nginx/nginx.crt -days 365 -subj "/C=Mo/ST=Oregon/L=Portland/O=ft_services/OU=Org/CN=www.sboof.com"
+mkdir /run/nginx
+cp /etc/nginx/default.conf /etc/nginx/conf.d/
+echo "sboof was here !!!" > /home/www/index.html
+/etc/init.d/nginx start
+tee -a
